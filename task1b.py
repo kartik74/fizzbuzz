cee500cbb0dafaad03709741de0d58c9c097e979
kartik-valecha <kr999760@dal.ca>
2020-02-20 15:34:15 -0400

    taskccommit

--- task1b.py
+++ task1b.py
@@ -1,9 +1,14 @@
-def install_year():
-    lst = []
-    c = open("Trail.csv", 'r')
-    for line in c:
-        words = line.split(','))
-        b = line[9]
-        if b == 'Good'
-            print()
-    for i in 
+def Active():
+    lst=[]
+    count=0
+    fin=open('Trails.csv')  
+    for line in fin:
+        words=line.split(",")
+        c=words[10]
+        if 'INST_YEAR' not in words[10]:
+            if c>2000:
+                lst.append(c)
+                count=count+1
+    return lst
+result=Active()
+print(result)
--- task1c.py
+++ task1c.py
@@ -1,14 +1,22 @@
-def Active():
-    lst=[]
+def ctiv():
     count=0
-    fin=open('Trails.csv')  
+    fin=open("Trail.csv", 'r')
     for line in fin:
         words=line.split(",")
-        c=words[10]
-        if 'INST_YEAR' not in words[10]:
-            if c>2000:
-                lst.append(c)
-                count=count+1
-    return lst
-result=Active()
-print(result)
+        c=words[12]
+        d=words[15]
+        e=words[23]
+        f=words[25]
+        if d == 'Active':
+            count = count +1
+            print("status" + count)
+            continue
+        elif c == 'Yes'
+            count1 = count1 +1    
+            print("lightning Roads are:" + count1)
+            continue
+        elif e == 'Yes' and f == 'Yes'
+            count2 = count2 +1
+            print("walking and biking  Roads are:" + count2)
+
+result=ctiv()
