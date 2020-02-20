d6a5a7672532383c88b06c4ab4610b72c762bf9b
kartik-valecha <kr999760@dal.ca>
2020-02-20 14:29:35 -0400

    initial commit

---
+++ task1b.py
@@ -0,0 +1,9 @@
+def install_year():
+    lst = []
+    c = open("Trail.csv", 'r')
+    for line in c:
+        words = line.split(','))
+        b = line[9]
+        if b == 'Good'
+            print()
+    for i in 
