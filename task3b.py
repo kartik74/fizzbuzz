667b46a6619ffe72223c9d53a6ceb9aa79d10fe8
kartik-valecha <kr999760@dal.ca>
2020-02-20 16:53:44 -0400

    uu

---
+++ task3b.py
@@ -0,0 +1,8 @@
+def count_the_article(lst):
+    count=0
+    count=len(lst)
+    return count
+
+lst=["a", "the", "at", "run", "to","and","are","or","for","an","this"]
+total=count_the article(lst)
+print(total)
